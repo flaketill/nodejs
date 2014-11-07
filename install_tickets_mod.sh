9fea6f2d587930a7664a5a81796eae597c0ad1f6
Ing. Armando Ibarra <armandoibarra1@gmail.com>
2014-11-07 13:01:26 -0700

    Init package base

---
+++ install_tickets_mod.sh
@@ -0,0 +1,29 @@
+#Don't forget
+#chmod u+x install.sh
+
+#Install express 
+npm install express-train
+
+#Install Yeoman 
+sudo npm install -g yo
+sudo npm install -g grunt-cli
+sudo npm install -g nodemon
+
+#Install Yeoman Generators
+sudo npm install -g generator-express-train
+
+#mkdir tecmilenio && cd tecmilenio
+
+bower install #& npm install 
+
+sudo npm install 
+sudo npm install  --save
+
+#initiate the generator (If you want build new app)
+#yo express-train
+npm start
+#node .
+
+#nodemon --debug server.js 8000
+nodemon --debug ./app.js localhost 3002
+#nodemon --debug ./app.js localhost 8080
